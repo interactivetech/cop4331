b7176797fcdb1fdf0f46d9ca72224600d6d8cf93
fellou89 <fellou89@hotmail.com>
2013-11-09 20:00:41 -0500

    fixed conflicts

--- src/com/example/pocket_monsters/EncounterService.java
+++ src/com/example/pocket_monsters/EncounterService.java
@@ -1,5 +1,20 @@
 package com.example.pocket_monsters;
 
-public class EncounterService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class EncounterService extends Service{
+
+	@Override
+	public void onCreate(){
+		
+	}
+	
+	@Override
+	public IBinder onBind(Intent arg0) {
+		
+		return null;
+	}
 
 }
